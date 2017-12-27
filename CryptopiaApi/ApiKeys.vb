4d2c4d5bc5d2e6525cb45a512b4931658e402c0a
Arvinder Sehmi <arvinder@adcmarketingltd.onmicrosoft.com>
2017-12-27 17:09:42 +0000

    Cleaned up key storage code

--- CryptopiaApi/ApiKeys.vb
+++ CryptopiaApi/ApiKeys.vb
@@ -1,8 +1,17 @@
 ﻿Public Class ApiKeys
 
-    Property API_KEY As String
-    Property API_SECRET_BYTES As Byte()
+    Private Const REG_KEY_ROOT As String = "HKEY_CURRENT_USER\Software\ArviSoft\CryptopiaApi"
+    Private Const REG_KEY As String = "ApiKeys"
 
+    Public Property API_KEY As String
+    Public Property API_SECRET_BYTES As Byte()
+
+    Public Shared Sub DeleteDefaults()
+        My.Computer.Registry.SetValue(REG_KEY_ROOT, REG_KEY, New Byte() {}, Microsoft.Win32.RegistryValueKind.Binary)
+    End Sub
+
+    Public Sub New()
+    End Sub
 
     Public Sub New(ApiKey As String, ApiSecret As String)
         API_KEY = ApiKey
@@ -16,12 +25,12 @@
     Public Sub StoreAsDefaults()
         Dim data = System.Text.UTF8Encoding.UTF8.GetBytes(Newtonsoft.Json.JsonConvert.SerializeObject(Me))
         Dim protectedData = System.Security.Cryptography.ProtectedData.Protect(data, Nothing, Security.Cryptography.DataProtectionScope.CurrentUser)
-        My.Computer.Registry.CurrentUser.SetValue($"CryptopiaApi\ApiKeys", protectedData, Microsoft.Win32.RegistryValueKind.Binary)
+        My.Computer.Registry.SetValue(REG_KEY_ROOT, REG_KEY, protectedData, Microsoft.Win32.RegistryValueKind.Binary)
     End Sub
 
     Public Shared Function LoadDefaults() As ApiKeys
 
-        Dim protectedKeys = TryCast(My.Computer.Registry.CurrentUser.GetValue($"CryptopiaApi\ApiKeys"), Byte())
+        Dim protectedKeys = TryCast(My.Computer.Registry.GetValue(REG_KEY_ROOT, REG_KEY, Nothing), Byte())
 
         If protectedKeys Is Nothing Then Return Nothing
         If protectedKeys.Length = 0 Then Return Nothing
