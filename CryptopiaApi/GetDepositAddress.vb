340c8f2ae35e46766fbc1e775aae5c9805c6fc6f
Arvinder Sehmi <arvinder@adcmarketingltd.onmicrosoft.com>
2018-01-05 11:29:13 +0000

    added annotations

--- CryptopiaApi/GetDepositAddress.vb
+++ CryptopiaApi/GetDepositAddress.vb
@@ -7,7 +7,9 @@
 
 
     Public Class Parameters
+        ''' <summary>The currency symbol of the address to return e.g. 'DOT' (not required if 'CurrencyId' supplied)</summary>
         Property Currency As String
+        ''' <summary>The Cryptopia currency identifier of the address to return e.g. '2' (not required if 'Currency' supplied)</summary>
         Property CurrencyId As Integer?
     End Class
 
